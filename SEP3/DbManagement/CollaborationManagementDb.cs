9fa57c0b6fe5872de48f4d90f2a2320891a22760
Lena Bojanowska <lena.bojanowska99@gmail.com>
2019-12-14 01:08:53 +0100

    commit

---
+++ SEP3/DbManagement/CollaborationManagementDb.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEP3.DbManagement
+{
+    public class CollaborationManagementDb
+    {
+    }
+}
