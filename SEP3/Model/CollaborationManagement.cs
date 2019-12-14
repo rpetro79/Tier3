9b9c2eae5af60b985284d9924e46ac27d8fed6b2
Lena Bojanowska <lena.bojanowska99@gmail.com>
2019-12-14 01:14:17 +0100

    c

--- SEP3/Model/CollaborationManagement.cs
+++ SEP3/Model/CollaborationManagement.cs
@@ -7,5 +7,23 @@ namespace SEP3.Model
 {
     public class CollaborationManagement
     {
+        public Collaboration Collaboration { get; set; }
+        public bool Closed { get; set; }
+        public List<ITProvider> ProvidersOnCollaboration { get; set; }
+
+        public CollaborationManagement() { }
+
+        public CollaborationManagement(Collaboration C, bool closed, List<ITProvider> poc)
+        {
+            this.Collaboration = C;
+            this.Closed = closed;
+            this.ProvidersOnCollaboration = poc;
+        }
+
+        public CollaborationManagement(Collaboration C)
+        {
+            this.Collaboration = C;
+            ProvidersOnCollaboration = new List<ITProvider>();
+        }
     }
 }
